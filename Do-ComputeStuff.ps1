1f276b4d682ac4d48a2e4f8bd25eb9085184a21b
lev <levidos@gmail.com>
2021-09-21 14:13:47 +0100

    Added Answer parameter

--- Do-ComputeStuff.ps1
+++ Do-ComputeStuff.ps1
@@ -1,8 +1,7 @@
 param(
-  [Parameter()]
-  [ValidateSet('Compute','MIG')]
-  [string[]]
-  $ResourceType = 'Compute'
+  [Parameter()][ValidateSet('Compute','MIG')][string[]]$ResourceType = 'Compute',
+  [Switch]$UseInternalIpSsh,
+  [string]$Answer
 )
 
 switch ($ResourceType) {
@@ -10,11 +9,11 @@ switch ($ResourceType) {
   "MIG" { $outputCmd="gcloud compute instance-groups managed list --format='csv(name,LOCATION,size)'"; break }
 }
 
-while ($ans -eq "") {
+do {
   $output=$(Invoke-Expression $outputCmd)
 
   if ($output.Count -eq 0) {
-    ThrowError "No $($ResourceType.ToLower()) instances find"
+    ThrowError "No $($ResourceType.ToLower()) instances found"
   }
 
   for ($i=0; $i -lt $output.Count; $i++) {
@@ -27,23 +26,27 @@ while ($ans -eq "") {
     }
   }
 
-  $instances=convertfrom-csv -InputObject $output
+  $instances=ConvertFrom-Csv -InputObject $output
 
-  $outText=($instances | Format-Table | Out-String).Replace("`r`n`r`n", "")
-  Write-Host $outText
-  Write-Host "S) SSH`tL) Serial port Log`tC) Start-up sCript log`tU) Update instance template`n"
+  if ([string]::IsNullOrEmpty($Answer)) {
+    $outText=($instances | Format-Table | Out-String).Replace("`r`n`r`n", "")
+    Write-Host $outText
+    Write-Host "S) SSH`tL) Serial port Log`tC) Start-up sCript log`tU) Update instance template`n"
 
-  $ans = Read-Host `n'Enter selection'
-}
+    $Answer = Read-Host `n'Enter selection'
+  }
 
-if ($ans -match '^\d$') {
+} while ([string]::IsNullOrEmpty($Answer))
+
+
+if ($Answer -match '^\d$') {
   # Number only, default action is ssh
   $action = "s" # SSH
-  [int]$item = $ans
+  [int]$item = $Answer
 }
-elseif ($ans -match '^[a-z]\d$') {
-  $action = $ans[0]
-  [int]$item = $ans.Substring(1)
+elseif ($Answer -match '^[a-z]\d$') {
+  $action = $Answer[0]
+  [int]$item = $Answer.Substring(1)
 }
 else {
   write-error "Unable to parse response."
@@ -54,8 +57,12 @@ $sel = $instances[$item-1] # Selection
 
 Write-Host "You selected: $sel`n"
 
+if($UseInternalIpSsh) {
+  $UseInternalIpCmd="--internal-ip"
+}
+
 switch ($action) {
-  "s" { $type="hcmd"; $argListMid = "compute ssh --internal-ip --zone=$($sel.zone) $($sel.name)"; break }
+  "s" { $type="hcmd"; $argListMid = "compute ssh $UseInternalIpCmd --zone=$($sel.zone) $($sel.name)"; break }
   "u" { $type="cmd"; $argListMid = "compute instance-groups managed update-instances --region=$($sel.zone -replace '..$') --minimal-action=replace $($sel.'created-by') --instances=$($sel.name)"; break }
   "l" { $type="log"; $argListMid = "compute instances get-serial-port-output --zone=$($sel.zone) $($sel.name)"; break }
   "c" { $type="log"; $argListMid = "compute instances get-serial-port-output --zone=$($sel.zone) $($sel.name) | grep startup-script"; break }
