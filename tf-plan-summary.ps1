63a936befa458fb90524e3fcc34e4099eacb5536
Levente Rog <Levente.Rog@natwestmarkets.com>
2021-12-15 08:49:23 +0000

    Enhanced formatting

--- tf-plan-summary.ps1
+++ tf-plan-summary.ps1
@@ -1,35 +1,98 @@
 param(
-  [Parameter(Position=0)][string]$Logfile
+  [Parameter(Position=0)][string]$Logfile='.\tf-plan.log',
+  [Parameter()][ValidateSet('Text','Object')][string[]]$OutputType = 'Text',
+  [Parameter()][string]$Grep= '',
+  [Switch]$RefreshPlan
 )
 
-$RegexPattern='(  \# (.*) (will|must) be .*(created|destroyed|replaced)| +(.*)\s\# forces replacement)'
+if ($RefreshPlan) {
+  if (Test-Path $LogFile) {
+    $LogFileAttr = Get-ItemProperty $LogFile
+
+    $TFPlansArchiveFolderName='old-tfplans'
+    $TFPlansArchiveFolderPath="$($LogFileAttr.DirectoryName)\$TFPlansArchiveFolderName"
+
+    if (!(Test-Path $TFPlansArchiveFolderPath)) {
+      New-Item -Path $TFPlansArchiveFolderPath -ItemType Directory
+    }
+
+    $LogTime = $LogFileAttr.LastWriteTime
+    $LogTimestamp = Get-Date $LogTime -format 'yyyyMMdd-hhmmss'
+    $LogRename = "$($LogFileAttr.BaseName)-$LogTimestamp$($LogFileAttr.Extension)"
+
+    Move-Item -Path $Logfile -Destination "$TFPlansArchiveFolderPath\$LogRename"
+  }
+}
+
+if (!(Test-Path $LogFile)) {
+  if ($RefreshPlan) { $UpdatedMsg = 'updated '} else { $UpdatedMsg = '' }
+  Write-Output "Generating $($UpdatedMsg)terraform plan..."
+  &tf plan -no-color > "$LogFile"
+
+  if ($LASTEXITCODE -ne 0) {
+    $Raise_Error = "Error executing Terraform Plan."
+    Throw $Raise_Error
+  }
+}
+
+$RegexPattern='(  \# (.*) (will|must) be .*(created|destroyed|replaced|updated)| +(.*)\s\# forces replacement| ~(.*) ->.*)'
 
 $wordColours = @{
   created = 'Green';
   destroyed = 'Red';
   replaced = 'Yellow';
-  replacement = 'DarkGray';  
+  replacement = 'Yellow';
+  updated = 'White'
   '#' = 'DarkGray';
   will = 'DarkGray';
   must = 'DarkGray';
   be = 'DarkGray';
-  forces = 'DarkGray';
+  forces = 'Yellow';
   '' = 'White'
 }
 
+$wordPadded = @{
+  created   = '   ';
+  destroyed = '  ';
+  replaced  = '  ';
+  updated   = '   ';
+}
+
+$wordAction = @{
+  created   = 'create';
+  destroyed = 'destroy';
+  replaced  = 'replace';
+  updated   = 'update';
+}
+
+$PlanStart=Select-String $Logfile -Pattern 'Terraform will perform the following actions'
+if (!$PlanStart) {
+  $Raise_Error = "ERROR: No changes detected in the terraform plan."
+  Throw $Raise_Error
+}
+$PlanStartLine=$PlanStart[0].LineNumber
+$PlanEnd=Select-String $Logfile -Pattern 'Plan: (\d+) to add, (\d+) to change, (\d+) to destroy.'
+$PlanEndLine=$PlanEnd[0].LineNumber
 $Content = Select-String $Logfile -Pattern $RegexPattern
 
-$Resource = @{
-  name = '';
-  action = '';
-  fgcolor = 0;
-}
 $Resources = @()
 
+Write-Host ''
+
+$Content = $Content | Where-Object { ($_.LineNumber -gt $PlanStartLine) -and ($_.LineNumber -lt $PlanEndLine) }
+
+if ($Grep) {
+  $Content = $Content | Where-Object Line -match $Grep
+}
+
 $Content | ForEach-Object {
-  $ResourceName = $_.Matches.Groups[2].Value
+  $ResourceName = $_.Matches.Groups[2].Value -replace '\["', '[\"' -replace '"\]', '\"]'
   $Action = $_.Matches.Groups[4].Value
-  $Words = $_.Line -Split " "
+
+  if($Action) {
+    Write-Host "$($wordAction["$Action"])" -NoNewLine -ForegroundColor $wordColours["$Action"]
+    Write-Host "$($wordPadded["$Action"])" -NoNewLine -ForegroundColor DarkGray
+  }
 
   if($ResourceName) {
     $Resources += [pscustomobject]@{
@@ -37,18 +100,50 @@ $Content | ForEach-Object {
       Action = $Action;
       FGColor = $wordColours["$Action"]
     }
-    Write-Host "$ResourceName" -noNewLine
+    Write-Host "$ResourceName" -NoNewLine
   }
 
-  if($Action) {
-    Write-Host " -> " -NoNewLine -ForegroundColor DarkGray
-    Write-Host "$Action" -NoNewLine -ForegroundColor $wordColours["$Action"]
-  }
   if(!$ResourceName) {
-    Write-Host " $($_.Line)" -NoNewLine -ForegroundColor DarkGray
+    $fgcolor = if ($_.Line | Select-String -Pattern ' # forces replacement') { $wordColours['replaced']  } else {  $wordColours['#'] }
+    Write-Host " $($_.Line)" -NoNewLine -ForegroundColor $fgcolor
   }
+
+  # if(!$ResourceName) {
+  #   Write-Host "`t" -NoNewLine
+  #   $_.Line -split ' ' | ForEach-Object {
+  #     $fgcolor = $wordColours["$_"]
+  #     if ($fgcolor) {
+  #       Write-Host " $_" -NoNewLine -ForegroundColor $fgcolor
+  #     }
+  #     else {
+  #       Write-Host " $_" -NoNewLine -ForegroundColor DarkGray
+  #     }
+  #   }
+  #   #Write-Host "`t $($_.Line)" -NoNewLine -ForegroundColor DarkGray
+  # }
 
   Write-Host ''
 }
 
-# return $Resources
+$Add = $PlanEnd.Matches.Groups[1].Value
+$Change = $PlanEnd.Matches.Groups[2].Value
+$Destroy = $PlanEnd.Matches.Groups[3].Value
+
+$AddColour = If ($Add -gt 0) {"created"} Else {"#"}
+$ChangeColour = If ($Change -gt 0) {"updated"} Else {"#"}
+$DestroyColour = If ($Destroy -gt 0) {"destroyed"} Else {"#"}
+
+Write-Host ''
+Write-Host 'Plan: ' -NoNewline
+Write-Host "$Add to add" -NoNewline -ForegroundColor $wordColours[$AddColour]
+Write-Host ', ' -NoNewline -ForegroundColor $wordColours["#"]
+Write-Host "$Change to change" -NoNewline -ForegroundColor $wordColours[$ChangeColour]
+Write-Host ', ' -NoNewline -ForegroundColor $wordColours["#"]
+Write-Host "$Destroy to destroy" -NoNewline -ForegroundColor $wordColours[$DestroyColour]
+Write-Host ".`n" -ForegroundColor $wordColours["#"]
+
+# Write-Host "Plan: $Add to add, $Change to change, $Destroy to destroy." -ForegroundColor White
+
+if ($OutputType -eq "Object") {
+  return $Resources
+}
