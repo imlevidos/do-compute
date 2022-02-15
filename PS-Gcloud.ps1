8b88f7da4a25308ebcf166e557d1d59d45f58253
Levente Rog <Levente.Rog@natwestmarkets.com>
2022-02-15 18:01:17 +0000

    Added multi target support and filter

--- PS-Gcloud.ps1
+++ PS-Gcloud.ps1
@@ -3,8 +3,11 @@ param(
   [nullable[bool]]$UseInternalIpSsh,
   [Parameter(Position=0)][string]$Answer,
   [Switch]$Install,
-  [Switch]${Show-Command}
+  [Switch]${Show-Command},
+  [Switch]${ReturnAs-Object}
 )
+
+$Sel=$null
 
 # $env:PATH="d:\src\do-compute;$env:path"
 if ($Install -eq $true) {
@@ -32,18 +35,19 @@ if ($Install -eq $true) {
 
 switch ($ResourceType) {
   "Compute" { 
-    $outputCmd="gcloud compute instances list --format='csv(name,zone,MACHINE_TYPE,INTERNAL_IP,EXTERNAL_IP,status,metadata.items[created-by].scope(instanceGroupManagers),id)'"; 
-    $instructions="[S]SH`t[O]UTPUT:serial-port `t[L]OG:start-up `t[T]AIL:start-up `t[U]PDATE:instance-template`t[R]ESET`t[P]OWER-OFF`t[D]ESCRIBE`t[Q]UIT"
-    $transform='Sort-Object -Property tmpregion,created-by,name'
+    $outputCmd="gcloud compute instances list --format='csv(name,zone,MACHINE_TYPE,INTERNAL_IP,EXTERNAL_IP,status,metadata.items[created-by].scope(instanceGroupManagers),id,networkInterfaces[0].subnetwork.scope(regions).segment(0):label=tmpregion,creationTimestamp.date(%Y-%m-%d %H:%M:%S):label=CreatedTime)'";
+    $instructions="[S]SH`tE[X#=cmd]ECUTE`t[C#=cmd]MD-INLINE`t[O]UTPUT-serial-log`t[T]AIL-STARTUP`t[U]PDATE-instance-template`t[R]ESET`t[P]OWER-OFF`t[D]ESCRIBE`t[Q]UIT"
+    $transform='Sort-Object -Property tmpregion,created-by,CreatedTime'
     break 
   }
   "MIG" { 
-    $outputCmd="gcloud compute instance-groups managed list --format='csv(name,LOCATION,size,autoHealingPolicies[0].healthCheck.scope(healthChecks):label='autoheal_hc')'";
+    $outputCmd="gcloud compute instance-groups managed list --format='csv(name,LOCATION,size,autoHealingPolicies[0].healthCheck.scope(healthChecks):label='autoheal_hc',creationTimestamp.date(%Y-%m-%d %H:%M:%S):label=CreatedTime)'";
     $instructions="[R#=#]ESIZE`t[D]ESCRIBE`t[U]PDATE`t[C]LEAR-AUTOHEALING`t[Q]UIT"
+    $transform='Sort-Object -Property location,name'
     break 
   }
   "backend-services" {
-    $outputCmd="gcloud compute backend-services list --format='csv(name,region.scope(regions),backends[0].group.scope(instanceGroups))'";
+    $outputCmd="gcloud compute backend-services list --format='csv(name,region.scope(regions),backends[0].group.scope(instanceGroups),creationTimestamp.date(%Y-%m-%d %H:%M:%S):label=CreatedTime)'";
     $instructions="[P]OOL:list`t[D]ESCRIBE`t[Q]UIT"
     break 
   }
@@ -53,7 +57,7 @@ switch ($ResourceType) {
     break
   }
   "SQL" {
-    $outputCmd="gcloud sql instances list --format='csv(name,database_version,gceZone:label='location',settings.availabilityType,settings.tier,ipAddresses[0].ipAddress,state,settings.dataDiskType,settings.dataDiskSizeGb)'";
+    $outputCmd="gcloud sql instances list --format='csv(name,database_version,gceZone:label='location',settings.availabilityType,settings.tier,ipAddresses[0].ipAddress,state,settings.dataDiskType:label=disk_type,settings.dataDiskSizeGb:label=disk_size,region:label=tmpregion,createTime.date(%Y-%m-%d %H:%M:%S):sort=1)'";
     $instructions="[B]ACKUP`t[L]IST-BACKUPS`t[Q]UIT"
     break
   }
@@ -61,57 +65,123 @@ switch ($ResourceType) {
 
 do {
   $output=$(Invoke-Expression $outputCmd)
-  $outputTmp=ConvertFrom-Csv -InputObject $output
-
-  if ($output.Count -eq 0) {
-    $Raise_Error = "No $($ResourceType.ToLower()) instances found in GCP project."
-    Throw $Raise_Error
+  if($LASTEXITCODE -ne 0) {
+    $Raise_Error = "Error running gcloud command"; Throw $Raise_Error
   }
 
-  for ($i=0; $i -lt $output.Count; $i++) {
-    if ($i -eq 0) {
-      # Append additional columns to object
-      $output[$i]="$($output[$i]),tmpregion"
-    }
-    else {
-      $region=$outputTmp[$i-1].Zone -replace ".{2}$"
-      $output[$i]="$($output[$i]),$region"
-    }
+  $outputTmp=ConvertFrom-Csv -InputObject $output
+  if ($outputTmp.Count -eq 0) {
+    $Raise_Error = "No $($ResourceType.ToLower()) instances found in GCP project."; Throw $Raise_Error
   }
 
   $instances = ConvertFrom-Csv -InputObject $output
-
   if($transform) {
     $instances = Invoke-Expression "`$instances | $transform"
   }
 
   $outText=($instances | Select-Object * -ExcludeProperty tmp* | ForEach-Object {$index=1} {$_; $index++} | Format-Table -Property @{ Label='index';Expression={$index}; },* | Out-String).Replace("`r`n`r`n", "")
 
-  if ([string]::IsNullOrEmpty($Answer) -or $Answer -eq 'q') {    
-    Write-Host $outText
-    Write-Host $instructions
+  if (${ReturnAs-Object} -eq $true) {
+    return $instances
   }
 
-  if ([string]::IsNullOrEmpty($Answer)) {    
-    $Answer = Read-Host `n'Enter selection'
+  if ([string]::IsNullOrEmpty($Answer) -or $Answer -eq 'q') {    
+    Write-Host $outText
+    Write-Host "$instructions`n"
+  }
+
+  if ([string]::IsNullOrEmpty($Answer)) {
+    $Answer = Read-Host 'Enter selection'
+  }
+
+  if($Answer -eq 'q' ) {
+    exit; break
   }
 
 } while ([string]::IsNullOrEmpty($Answer))
 
-$Answers = Select-String -InputObject $Answer -Pattern '([a-z]{1,2})?(\d+)?=?([\da-z\-]+)?' | select -ExpandProperty Matches | select -ExpandProperty Groups | select -ExpandProperty Value
-$Action = $Answers[1]
-$Item = $Answers[2]
-$Param = $Answers[3]
 
-if ($ResourceType -eq 'Configurations' -and $instances.Name -contains $answer) {
-  $action = 'a'
-  [int]$item = $instances | Where-Object Name -eq $answer | Select-Object -ExpandProperty Index
+if($sel -eq $null) {
+  # Lookup phase 0 - gcloud config configurations shortcut
+  if ($ResourceType -eq 'Configurations') {
+    $sel = $instances | where name -like "*$Answer*"
+    $action = 'a'
+
+    if(($sel | measure).Count -eq 0) {
+      $Raise_Error = "Filter *$Answer* found no matching entries." ; Throw $Raise_Error     
+    }
+    elseif(($sel | measure).Count -gt 1) {
+      $Raise_Error = "Filter *$Answer* matched multiple entries, pls narrow." ; Throw $Raise_Error     
+    }
+  }
 }
 
-if ([int]$Item -gt 0) {
-  $sel = $instances[$item-1] # Selection
-  Write-Host "Your selection: $sel`n"
+if($sel -eq $null) {
+  # Lookup phase 1
+  $Answers = Select-String -InputObject $Answer -Pattern '^([a-z]{1})?((\d{1,2}))?(=(.+))?$' | select -ExpandProperty Matches | select -ExpandProperty Groups | select -ExpandProperty Value
+
+ 
+  if ($Answers -ne $null) {
+    $Action = $Answers[1]
+    $Item = $Answers[3]
+    $Param = $Answers[5]
+
+    if([string]::IsNullOrEmpty($Item)) {
+      $Raise_Error = "ERROR: No item selected." ; Throw $Raise_Error     
+    }
+  
+    $sel = $instances[$item-1] # Selection
+    Write-Host "Your selection: $sel`n"
+  }
 }
+
+if($sel -eq $null) {
+  # Lookup phase 2
+  $Answers = Select-String -InputObject $Answer -Pattern '^([a-z]{1})?(:([\da-z\-\*]+))?(=(.+))?$' | select -ExpandProperty Matches | select -ExpandProperty Groups | select -ExpandProperty Value
+
+  $Action = $Answers[1]
+  $Filter = $Answers[3]
+  $Param = $Answers[5]
+
+  $sel = $instances | where name -ilike "*$Filter*"
+
+  if(($sel | measure).Count -eq 0) {
+    $Raise_Error = "Filter *$Filter* found no matching entries." ; Throw $Raise_Error     
+  }
+  elseif(($sel | measure).Count -gt 1) {
+    Write-Host "`nYour selections:"
+    $sel.Name | % { "- $_" }
+    Write-Host ""
+    # Do nothing here but confirm with user at the enxt step.
+  }
+  else {
+    Write-Host "Your selection: $sel`n"
+  }
+}
+
+# if ($Answers -eq $null) {
+# # Answer is a search string
+#   $result = $instances | where name -ilike "*$answer*"
+#   if(($result | measure).Count -eq 0) {
+#     $Raise_Error = "Filter *$answer* found no matching entries." ; Throw $Raise_Error     
+#   }
+#   elseif(($result | measure).Count -gt 1) {
+#     $Raise_Error = "Filter *$answer* matched multiple entries, pls narrow." ; Throw $Raise_Error     
+#   }
+#   else {
+#     # All good
+#   }
+# }
+# else {
+#   $Action = $Answers[1]
+#   $Item = $Answers[2]
+#   $Param = $Answers[3]  
+# }
+
+# if ($ResourceType -eq 'Configurations' -and $instances.Name -contains $answer) {
+#   $action = 'a'
+#   [int]$item = $instances | Where-Object Name -eq $answer | Select-Object -ExpandProperty Index
+# }
 
 if ($UseInternalIpSsh -eq $null) {
   # -UseInternalIpSsh switch not present, use proactive detection  
@@ -129,68 +199,87 @@ if ($UseInternalIpSsh) {
   $UseInternalIpCmd="--internal-ip"
 }
 
-switch -regex ("$ResourceType`:$action") {
-  ".*:q$" { exit; break }
-  "Compute:s?$" { $type="hcmd"; $argListMid = "compute ssh $UseInternalIpCmd --zone=$($sel.zone) $($sel.name)"; break }
-  "Compute:u" { $type="cmd"; $argListMid = "compute instance-groups managed update-instances --region=$($sel.zone -replace '..$') --minimal-action=replace $($sel.'created-by') --instances=$($sel.name)"; break }
-  "Compute:r" { $type="cmd"; $argListMid = "compute instances reset --zone=$($sel.zone) $($sel.name)"; break }
-  "Compute:p" { $type="cmd"; $argListMid = "compute instances stop --zone=$($sel.zone) $($sel.name)"; break }
-  "Compute:o" { $type="log"; $argListMid = "compute instances get-serial-port-output --zone=$($sel.zone) $($sel.name)"; break }
-  "Compute:l" { $type="log"; $argListMid = "compute instances get-serial-port-output --zone=$($sel.zone) $($sel.name) | grep startup-script"; break }
-  "Compute:d" { $type="inline"; $argListMid = "compute instances describe --zone=$($sel.zone) $($sel.name)"; break }
-  "Compute:t" { $type="log"; $argListMid = "beta logging tail `"resource.type=gce_instance AND resource.labels.instance_id=$($sel.id)`" --format=`"value(format('$($sel.name):{0}',json_payload.message).sub(':startup-script:',':'))`""; break }
-  "Compute:ta" { $type="log"; $argListMid = "beta logging tail `"resource.type=gce_instance`" --format=`"value(format('{0}:{1}',resource.labels.instance_id,json_payload.message).sub(':startup-script:',':'))`""; break }  
-  "MIG:r" { $type="cmd"; $argListMid = "compute instance-groups managed resize $($sel.name) --region=$($sel.location) --size=$($param)"; break }
-  "MIG:u" { $type="cmd"; $argListMid = "compute instance-groups managed rolling-action replace $($sel.name) --region=$($sel.location)"; break }
-  "MIG:c" { $type="cmd"; $argListMid = "compute instance-groups managed update --clear-autohealing  $($sel.name) --region=$($sel.location)"; break }
-  "MIG:d" { $type="inline"; $argListMid = "compute instance-groups managed describe $($sel.name) --region=$($sel.location)"; break }
-  "SQL:l" { $type="inline"; $argListMid = "sql backups list --instance=$($sel.name)"; break }
-  "SQL:b" { $type="inline"; $argListMid = "sql backups create --instance=$($sel.name)"; break }
-  "backend-services:p?$" { $type="inline"; $argListMid = "compute backend-services get-health $($sel.name) --region=$($sel.region) --format='table(status.healthStatus.instance.scope(instances),status.healthStatus.instance.scope(zones).segment(0):label='zone',status.healthStatus.ipAddress,status.healthStatus.healthState)' --flatten='status.healthStatus'"; break }
-  "backend-services:d$" { $type="inline"; $argListMid = "compute backend-services describe $($sel.name) --region=$($sel.region) --format=yaml"; break }
-  "Configurations:a?$" {  $type="inline"; $argListMid = "config configurations activate $($sel.name)"; break }
-  default { $Raise_Error = "No action defined for ``$ResourceType`:$action``" ; Throw $Raise_Error }
-}
-
-
-$HAVE_CONEMU=$true # TBC - Add conemu detection
-
-# Default shell
-$shell = "cmd"
-$shellParams = "/c"
-$windowStyle = "Normal"
-$SleepCmd = "& timeout /t 60"
-
-if (${Show-Command} -eq $true) {
-  $shellParams = "COMMAND:"
-  $SleepCmd = ""
-}
-elseif ($type -eq "inline") {
-  $shellParams = ""
-  $SleepCmd = ""
-}
-elseif ($type -eq "hcmd") {
-  $windowStyle = "Minimized"
-}
-elseif ($type -eq "log") {
-  $windowStyle = "Maximized"
-  $SleepCmd = "& pause"
-  if ($HAVE_CONEMU) {
-    $shell = "conemu64"
-    $shellParams = "-run"
+$selCount=$sel.Count
+if($selCount -gt 1 -and ${Show-Command} -eq $false) {
+  $YesNo = Read-Host "WARNING: Execute on multiple targets? (yes/no)"
+  Write-Host ""
+  if (@('y','yes') -notcontains $YesNo) {
+    exit; break
   }
 }
 
-$argList = "$shellParams gcloud $argListMid $SleepCmd"
+foreach ($sel in $sel) {
+  if($selCount -gt 1 -and ${Show-Command} -eq $false) {
+    Write-Host "[PS-GCLOUD] Processing: $($sel.name)...`n"
+  }
+  switch -regex ("$ResourceType`:$action") {
+    ".*:q$" { exit; break }
+    "Compute:s?$" { $type="hcmd"; $argListMid = "compute ssh $UseInternalIpCmd --zone=$($sel.zone) $($sel.name)"; break }
+    "Compute:u" { $type="cmd"; $argListMid = "compute instance-groups managed update-instances --region=$($sel.zone -replace '..$') --minimal-action=replace $($sel.'created-by') --instances=$($sel.name)"; break }
+    "Compute:r" { $type="cmd"; $argListMid = "compute instances reset --zone=$($sel.zone) $($sel.name)"; break }
+    "Compute:p" { $type="cmd"; $argListMid = "compute instances stop --zone=$($sel.zone) $($sel.name)"; break }
+    "Compute:o" { $type="log"; $argListMid = "compute instances get-serial-port-output --zone=$($sel.zone) $($sel.name)"; break }
+    "Compute:l" { $type="log"; $argListMid = "compute ssh $UseInternalIpCmd --zone=$($sel.zone) $($sel.name) --command=`"sudo journalctl -xefu google-startup-scripts.service`""; break }
+    "Compute:x" { $type="log"; $argListMid = "compute ssh $UseInternalIpCmd --zone=$($sel.zone) $($sel.name) --command `"$($param)`""; break }
+    "Compute:c" { $type="inline"; $argListMid = "compute ssh $UseInternalIpCmd --zone=$($sel.zone) $($sel.name) --command `"$($param)`""; break }
+    "Compute:d" { $type="inline"; $argListMid = "compute instances describe --zone=$($sel.zone) $($sel.name)"; break }
+    # "Compute:t" { $type="log"; $argListMid = "beta logging tail `"resource.type=gce_instance AND resource.labels.instance_id=$($sel.id)`" --format=`"value(format('$($sel.name):{0}',json_payload.message).sub(':startup-script:',':'))`""; break }
+    "Compute:ta" { $type="log"; $argListMid = "beta logging tail `"resource.type=gce_instance`" --format=`"value(format('{0}:{1}',resource.labels.instance_id,json_payload.message).sub(':startup-script:',':'))`""; break }  
+    "MIG:r" { $type="cmd"; $argListMid = "compute instance-groups managed resize $($sel.name) --region=$($sel.location) --size=$($param)"; break }
+    "MIG:u" { $type="cmd"; $argListMid = "compute instance-groups managed rolling-action replace $($sel.name) --region=$($sel.location)"; break }
+    "MIG:c" { $type="cmd"; $argListMid = "compute instance-groups managed update --clear-autohealing  $($sel.name) --region=$($sel.location)"; break }
+    "MIG:d" { $type="inline"; $argListMid = "compute instance-groups managed describe $($sel.name) --region=$($sel.location)"; break }
+    "SQL:l" { $type="inline"; $argListMid = "sql backups list --instance=$($sel.name)"; break }
+    "SQL:b" { $type="inline"; $argListMid = "sql backups create --instance=$($sel.name)"; break }
+    "backend-services:p?$" { $type="inline"; $argListMid = "compute backend-services get-health $($sel.name) --region=$($sel.region) --format='table(status.healthStatus.instance.scope(instances),status.healthStatus.instance.scope(zones).segment(0):label='zone',status.healthStatus.ipAddress,status.healthStatus.healthState)' --flatten='status.healthStatus'"; break }
+    "backend-services:d$" { $type="inline"; $argListMid = "compute backend-services describe $($sel.name) --region=$($sel.region) --format=yaml"; break }
+    "Configurations:a?$" {  $type="inline"; $argListMid = "config configurations activate $($sel.name)"; break }
+    default { $Raise_Error = "No action defined for ``$ResourceType`:$action``" ; Throw $Raise_Error }
+  }
+
+
+  $HAVE_CONEMU=$true # TBC - Add conemu detection
+
+  # Default shell
+  $shell = "cmd"
+  $shellParams = "/c"
+  $windowStyle = "Normal"
+  $SleepCmd = "& timeout /t 60"
+
+  if (${Show-Command} -eq $true) {
+    $shellParams = "COMMAND:"
+    $SleepCmd = ""
+  }
+  elseif ($type -eq "inline") {
+    $shellParams = ""
+    $SleepCmd = ""
+  }
+  elseif ($type -eq "hcmd") {
+    $windowStyle = "Minimized"
+  }
+  elseif ($type -eq "log") {
+    $windowStyle = "Maximized"
+    $SleepCmd = "& pause"
+    if ($HAVE_CONEMU) {
+      $shell = "conemu64"
+      $shellParams = "-run"
+    }
+  }
+
+  $argList = "$shellParams gcloud $argListMid $SleepCmd"
+
+  if (${Show-Command} -eq $true) {
+    Write-Host "$argList"
+  }
+  elseif ($type -eq "inline") {
+    Invoke-Expression "& $argList"
+    Write-Host ''
+  }
+  else {
+    Start-Process $shell -ArgumentList "$argList " -WindowStyle $windowStyle
+  }
+}
 
 if (${Show-Command} -eq $true) {
-  Write-Host "$argList`n"
+  Write-Host ""
 }
-elseif ($type -eq "inline") {
-  Invoke-Expression "& $argList"
-  Write-Host ''
-}
-else {
-  Start-Process $shell -ArgumentList "$argList " -WindowStyle $windowStyle
-}
-
